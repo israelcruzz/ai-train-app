04788cc379ca396ada57db007fdd0ac48ffdcb77
israelcruzz <israelcruz.contato@gmail.com>
2024-05-03 20:29:58 -0300

    feat: add layout for all screens

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Slot } from "expo-router";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+export default function Layout() {
+  const [fontLoading] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  return <>{fontLoading ? <Slot /> : ""}</>;
+}
