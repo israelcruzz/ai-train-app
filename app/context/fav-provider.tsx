e138c9ad238c485830703531dabfe922e55d36f8
israelcruzz <israelcruz.contato@gmail.com>
2024-05-04 18:02:45 -0300

    feat: add async storage in context

--- app/context/fav-provider.tsx
+++ app/context/fav-provider.tsx
@@ -1,8 +1,43 @@
-import { ReactNode, useContext, useState } from "react";
+import { ReactNode, useContext, useEffect, useState } from "react";
 import { FavContext } from "./fav-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 
 export function FavProvider({ children }: { children: ReactNode }) {
-  const [favoriteTrains, setFavoriteTrains] = useState<string[]>(["Treino de Superiores:"]);
+  const [favoriteTrains, setFavoriteTrains] = useState<string[]>([]);
+
+  const getFavoriteTrainsInMemory = async () => {
+    try {
+      let trainsInMemory = await AsyncStorage.getItem("@train-favorite");
+
+      if (trainsInMemory !== null) {
+        setFavoriteTrains(JSON.parse(trainsInMemory));
+      }
+    } catch (error) {
+      Alert.alert("Não foi possivel realizar a busca.", "Erro Interno");
+    }
+  };
+
+  const addTrainInMemory = async (train: string) => {
+    const existingTrainsInMemory: string | null = await AsyncStorage.getItem(
+      "@train-favorite"
+    );
+
+    const favoriteTrainsArray: string[] = existingTrainsInMemory
+      ? JSON.parse(existingTrainsInMemory)
+      : [];
+
+    favoriteTrainsArray.push(train);
+
+    await AsyncStorage.setItem(
+      "@train-favorite",
+      JSON.stringify(favoriteTrainsArray)
+    );
+  };
+
+  useEffect(() => {
+    getFavoriteTrainsInMemory();
+  }, []);
 
   const addFavoriteTrain = (train: string) => {
     setFavoriteTrains((prev) => {
@@ -17,6 +52,8 @@ export function FavProvider({ children }: { children: ReactNode }) {
         return trains;
       }
 
+      addTrainInMemory(train);
+
       return [...prev, train];
     });
   };
@@ -28,7 +65,7 @@ export function FavProvider({ children }: { children: ReactNode }) {
         train.toLocaleLowerCase().trim()
     );
 
-    return existTrain ? true : false
+    return existTrain ? true : false;
   };
 
   return (
